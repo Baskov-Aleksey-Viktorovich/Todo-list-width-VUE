28aaca6e5c9a2dc4e3dffa9731f2e78002fddbd0
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2022-12-28 10:35:01 +0200

    again sintax erorr

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <div class="task__block" v-for="todo in todos" :key="todo.id">
-    <span class="task__block__title">{{ todo.name }}</span>
-    <p class="task__block__desc">{{ todo.description }}</p>
+      <span class="task__block__title">{{ todo.name }}</span>
+      <p class="task__block__desc">{{ todo.description }}</p>
+    </div>
   </div>
-    </div>  
 </template>
 
 
 <script>
-export default{
+
+export default {
   props: {
     todos: {
       type: Array
-      }
+    }
   }
 }
 </script>
 
 <style>
-
 .task__block {
   width: 320px;
   background-color: #fff;
