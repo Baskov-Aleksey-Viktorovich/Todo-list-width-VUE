f43af17c1afeed049ace50519f94dfbe7b13c2bf
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2023-01-07 21:46:21 +0200

    merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
         'vue',
     ],
     rules: {
-    // allow async-await
+        // allow async-await
         'generator-star-spacing': 'off',
         // allow paren-less arrow functions
         'arrow-parens': 'off',
