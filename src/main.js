8ccea0b5a0233cf664d389eada34ec00b0746bb9
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2023-05-08 00:14:13 +0300

    feat: install router-vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 
+/* import { createRouter, createWebHistory } from 'vue-router'; */
+
+/* const router = createRouter({
+    history: createWebHistory(),
+}); */
+
 const app = createApp(App);
+/* app.use(router); */
 app.component('the-header', TheHeader);
 app.mount('#app');
